f7ebd865dad5df57940107fcca624115be3bb2e6
Lee_Sa <65063569+pauljerricho@users.noreply.github.com>
2024-06-04 13:27:52 +0900

    C#_Study_Winform
    Winform_2

---
+++ Study_C#/Study_3_Method/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Study_3_Method
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tboxResult = new System.Windows.Forms.TextBox();
+            this.tbox1 = new System.Windows.Forms.TextBox();
+            this.tbox2 = new System.Windows.Forms.TextBox();
+            this.btn_plus = new System.Windows.Forms.Button();
+            this.btn_minus = new System.Windows.Forms.Button();
+            this.btn_mul = new System.Windows.Forms.Button();
+            this.btn_div = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tboxResult
+            // 
+            this.tboxResult.Enabled = false;
+            this.tboxResult.Location = new System.Drawing.Point(78, 36);
+            this.tboxResult.Name = "tboxResult";
+            this.tboxResult.Size = new System.Drawing.Size(318, 25);
+            this.tboxResult.TabIndex = 0;
+            this.tboxResult.Text = "0";
+            // 
+            // tbox1
+            // 
+            this.tbox1.Location = new System.Drawing.Point(78, 76);
+            this.tbox1.Name = "tbox1";
+            this.tbox1.Size = new System.Drawing.Size(156, 25);
+            this.tbox1.TabIndex = 0;
+            this.tbox1.Text = "0";
+            // 
+            // tbox2
+            // 
+            this.tbox2.Location = new System.Drawing.Point(240, 76);
+            this.tbox2.Name = "tbox2";
+            this.tbox2.Size = new System.Drawing.Size(156, 25);
+            this.tbox2.TabIndex = 0;
+            this.tbox2.Text = "0";
+            // 
+            // btn_plus
+            // 
+            this.btn_plus.Location = new System.Drawing.Point(78, 132);
+            this.btn_plus.Name = "btn_plus";
+            this.btn_plus.Size = new System.Drawing.Size(75, 56);
+            this.btn_plus.TabIndex = 1;
+            this.btn_plus.Text = "+";
+            this.btn_plus.UseVisualStyleBackColor = true;
+            this.btn_plus.Click += new System.EventHandler(this.btn_plus_Click);
+            // 
+            // btn_minus
+            // 
+            this.btn_minus.Location = new System.Drawing.Point(159, 132);
+            this.btn_minus.Name = "btn_minus";
+            this.btn_minus.Size = new System.Drawing.Size(75, 56);
+            this.btn_minus.TabIndex = 1;
+            this.btn_minus.Text = "-";
+            this.btn_minus.UseVisualStyleBackColor = true;
+            this.btn_minus.Click += new System.EventHandler(this.btn_minus_Click);
+            // 
+            // btn_mul
+            // 
+            this.btn_mul.Location = new System.Drawing.Point(240, 132);
+            this.btn_mul.Name = "btn_mul";
+            this.btn_mul.Size = new System.Drawing.Size(75, 56);
+            this.btn_mul.TabIndex = 1;
+            this.btn_mul.Text = "*";
+            this.btn_mul.UseVisualStyleBackColor = true;
+            this.btn_mul.Click += new System.EventHandler(this.btn_mul_Click);
+            // 
+            // btn_div
+            // 
+            this.btn_div.Location = new System.Drawing.Point(321, 132);
+            this.btn_div.Name = "btn_div";
+            this.btn_div.Size = new System.Drawing.Size(75, 56);
+            this.btn_div.TabIndex = 1;
+            this.btn_div.Text = "/";
+            this.btn_div.UseVisualStyleBackColor = true;
+            this.btn_div.Click += new System.EventHandler(this.btn_div_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 283);
+            this.Controls.Add(this.btn_div);
+            this.Controls.Add(this.btn_mul);
+            this.Controls.Add(this.btn_minus);
+            this.Controls.Add(this.btn_plus);
+            this.Controls.Add(this.tbox2);
+            this.Controls.Add(this.tbox1);
+            this.Controls.Add(this.tboxResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tboxResult;
+        private System.Windows.Forms.TextBox tbox1;
+        private System.Windows.Forms.TextBox tbox2;
+        private System.Windows.Forms.Button btn_plus;
+        private System.Windows.Forms.Button btn_minus;
+        private System.Windows.Forms.Button btn_mul;
+        private System.Windows.Forms.Button btn_div;
+    }
+}
+
---
+++ Study_C#/Study_3_Method/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Study_3_Method
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_plus_Click(object sender, EventArgs e)
+        {
+            int iNumA = int.Parse(tbox1.Text);
+            int iNumB = int.Parse(tbox2.Text);
+
+            // int iResult = iNumA + iNumB;
+            // tboxResult.Text = iResult.ToString();
+
+            tboxResult.Text = fPlus(iNumA, iNumB).ToString(); // 숫자 -> 문자
+        }
+
+        private void btn_minus_Click(object sender, EventArgs e)
+        {
+            int iNumA = int.Parse(tbox1.Text);
+            int iNumB = int.Parse(tbox2.Text);
+
+            // int iResult = iNumA - iNumB;
+            // tboxResult.Text = iResult.ToString();
+
+            tboxResult.Text = fMinus(iNumA, iNumB).ToString();
+        }
+
+        private void btn_mul_Click(object sender, EventArgs e)
+        {
+            int iNumA = int.Parse(tbox1.Text);
+            int iNumB = int.Parse(tbox2.Text);
+
+            // int iResult = iNumA * iNumB;
+            // tboxResult.Text = iResult.ToString();
+
+            tboxResult.Text = fMul(iNumA, iNumB).ToString();
+        }
+
+        private void btn_div_Click(object sender, EventArgs e)
+        {
+            int iNumA = int.Parse(tbox1.Text);
+            int iNumB = int.Parse(tbox2.Text);
+
+            // int iResult = iNumA / iNumB;
+            // tboxResult.Text = iResult.ToString();
+
+            tboxResult.Text = fdiv(iNumA, iNumB).ToString();
+        }
+
+
+
+
+
+        private int fPlus(int iNumA, int iNumB)
+        {
+            // 반환 값을 적어줄 때는 초기화 값을 적어주는게 좋다.
+            int iResult = 0;
+
+            iResult = iNumA + iNumB;
+
+            return iResult;
+        }
+
+        private int fMinus(int iNumA, int iNumB)
+        {
+            // 반환 값을 적어줄 때는 초기화 값을 적어주는게 좋다.
+            int iResult = 0;
+
+            iResult = iNumA - iNumB;
+
+            return iResult;
+        }
+
+        private int fMul(int iNumA, int iNumB)
+        {
+            // 반환 값을 적어줄 때는 초기화 값을 적어주는게 좋다.
+            int iResult = 0;
+
+            iResult = iNumA * iNumB;
+
+            return iResult;
+        }
+
+        private int fdiv(int iNumA, int iNumB)
+        {
+            // 반환 값을 적어줄 때는 초기화 값을 적어주는게 좋다.
+            int iResult = 0;
+
+            iResult = iNumA / iNumB;
+
+            return iResult;
+        }
+    }
+}
